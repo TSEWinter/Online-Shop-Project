d58d4c2aa74c9e1e724915191cf479d01e2ceb30
TSEWinter <157279786+TSEWinter@users.noreply.github.com>
2026-01-09 10:38:55 +0800

    Шинэ хуудас нэмэв "Дэлгүүр"
    Header, footer хийгдсэн одоо хоосон хуудас.

---
+++ shop.php
@@ -0,0 +1,42 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Дэлгүүр</title>
+
+     <!-- font awesome cdn link -->
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
+     <!-- custom css file link -->
+     <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+
+     <!-- Header section -->
+     <?php include 'header.php'; ?>
+
+
+
+
+
+
+
+
+
+     <!-- footer section -->
+     <?php include 'footer.php'; ?>
+
+     <!-- JS file link -->
+     <script src="js/script.js"></script>
+
+</body>
+
+</html>
