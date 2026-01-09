c7c058ac29b119325d4a887b656341b91af5063b
TSEWinter <157279786+TSEWinter@users.noreply.github.com>
2026-01-09 14:55:55 +0800

    admin header засвар

--- admin/admin_header.php
+++ admin/admin_header.php
@@ -11,24 +11,8 @@ if (isset($message)) {
 }
 ?>
 
-<header class="header">
-
-     <div class="header">
-
-          <a href="dashboard.php">Admin Dashboard</a>
-
-          <div class="nav">
-               <a href="products.php">Бараа</a>
-               <a href="../home.php">Сайт үзэх</a>
-               <a href="../logout.php">Гарах</a>
-          </div>
-
-     </div>
-
-</header>
-
 <div class="header">
-     <div><b>Admin Dashboard</b></div>
+     <div><b><a href="dashboard.php">Admin Dashboard</a></b></div>
      <div class="nav">
           <a href="product.php">Products</a>
           <a href="../home.php">View Site</a>
--- admin/product.php
+++ admin/product.php
@@ -1,34 +1,55 @@
 <?php
 include '../config.php';
 session_start();
-if(!isset($_SESSION['admin_id'])){ header('Location: ../login.php'); exit; }
+if (!isset($_SESSION['admin_id'])) {
+     header('Location: ../login.php');
+     exit;
+}
 
-$products=mysqli_query($conn,"SELECT * FROM products");
+$products = mysqli_query($conn, "SELECT * FROM products");
 ?>
+
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Бүтээгдэхүүн</title>
+
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+
+     <link rel="stylesheet" href="/Online-Shop-Project/css/admin_style.css">
+</head>
+
 <body>
+     <?php include 'admin_header.php'; ?>
 
-<h2>Products</h2>
-<a href="product_add.php">+ Add Product</a>
-<table border="1" cellpadding="10">
-<tr>
-<th>Зураг</th><th>Нэр</th><th>Үнэ</th><th>Action</th>
-</tr>
 
-<?php while($p=mysqli_fetch_assoc($products)): ?>
-<tr>
-<td><img src="../uploaded_img/<?= $p['image'] ?>" width="80"></td>
-<td><?= $p['name'] ?></td>
-<td><?= $p['price'] ?></td>
-<td>
-<a href="product_edit.php?id=<?= $p['id'] ?>">Edit</a> |
-<a href="product_delete.php?id=<?= $p['id'] ?>" 
-onclick="return confirm('Устгах уу?')">Delete</a>
-</td>
-</tr>
-<?php endwhile; ?>
-</table>
+     <h2>Products</h2>
+     <a href="product_add.php">+ Add Product</a>
+     <table border="1" cellpadding="10">
+          <tr>
+               <th>Зураг</th>
+               <th>Нэр</th>
+               <th>Үнэ</th>
+               <th>Action</th>
+          </tr>
+
+          <?php while ($p = mysqli_fetch_assoc($products)): ?>
+               <tr>
+                    <td><img src="../uploaded_img/<?= $p['image'] ?>" width="80"></td>
+                    <td><?= $p['name'] ?></td>
+                    <td><?= $p['price'] ?></td>
+                    <td>
+                         <a href="product_edit.php?id=<?= $p['id'] ?>">Edit</a> |
+                         <a href="product_delete.php?id=<?= $p['id'] ?>"
+                              onclick="return confirm('Устгах уу?')">Delete</a>
+                    </td>
+               </tr>
+          <?php endwhile; ?>
+     </table>
 
 </body>
+
 </html>
